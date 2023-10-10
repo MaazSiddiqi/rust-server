31610cf9333baecaaa3f44345113c7e8927b43f0
Maaz Ali <maazali22@gmail.com>
2023-10-10 13:54:29 -0400

    added message broadcasting to all clients

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use tokio::{net::TcpListener, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}};
+use tokio::{net::TcpListener, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}, sync::broadcast};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (_tx, mut _rx) = broadcast::channel::<String>(100);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+        let tx = _tx.clone();
+        let mut rx = _tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
+
 
             loop {
                 let bytes_read = reader.read_line(&mut line).await.unwrap();
@@ -20,7 +25,11 @@ async fn main() {
                     break;
                 }
 
-                writer.write_all(line.as_bytes()).await.unwrap();
+                tx.send(line.clone()).unwrap();
+
+                let msg = rx.recv().await.unwrap();
+
+                writer.write_all(msg.as_bytes()).await.unwrap();
                 line.clear();
             }
         });
